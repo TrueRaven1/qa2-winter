fa841b1fc6c30756c039b9748e9214902a657b69
Deniss Bubins <deniss1@inbox.ru>
2021-10-27 21:08:56 +0300

    Initial commit

---
+++ src/test/java/homeWorks/FuelCalculation.java
@@ -0,0 +1,22 @@
+package homeWorks;
+
+import org.junit.jupiter.api.Test;
+
+public class FuelCalculation {
+
+    private float theAmountOfFuelForTheTrip(float a, float b) {
+
+        return a * b / 100;
+    }
+
+    @Test
+    public void test() {
+        float a = theAmountOfFuelForTheTrip(7, 150);
+        float b = theAmountOfFuelForTheTrip(8, 590);
+        float c = theAmountOfFuelForTheTrip(4, 220);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
