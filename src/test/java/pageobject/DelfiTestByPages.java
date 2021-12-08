c230e2b8e70a3e6f8a3b2d0daa058b8e534ab192
Deniss Bubins <deniss1@inbox.ru>
2021-12-08 13:51:20 +0200

    Home work delfi pages

--- src/test/java/pageobject/DelfiTestByPages.java
+++ src/test/java/pageobject/DelfiTestByPages.java
@@ -18,73 +18,30 @@ public class DelfiTestByPages {
 
     @Test
     public void titlesCheck() {
-        //Open browser window (maximize it)
-//        BaseFunc baseFunc = new BaseFunc();
-        //Open home page
         baseFunc.openUrl(HOME_PAGE_URL);
         HomePage homePage = new HomePage(baseFunc);
-        //Accept cookies
+
+        List<WebElement> titles = homePage.getTitlesOnDelfi();
+        for (WebElement title : titles) {
+            System.out.println(title.getText());
+        }
+        System.out.println("--------Printed all titles------------");
+
         homePage.acceptCookies();
-        //Get 1rd article title
         String titleText = homePage.getTitleOnDelfi(articleId);
-        //Print to console
         System.out.println(titleText);
-        //Open 1rd article
         homePage.openArticleByIdDelfi(articleId);
         ArticlePage articlePage = new ArticlePage(baseFunc);
-        //Get title
         String titleTextInArticle = articlePage.getTitleInArticleDelfi();
-        //Print to console
         System.out.println(titleTextInArticle);
-        //Compare titles
         Assertions.assertTrue(titleText.startsWith(titleTextInArticle), "Titles are not the same");
-        //Go to comment page
         articlePage.openCommentPageByLocator(articleId);
         CommentPage commentPage = new CommentPage(baseFunc);
-        //compare titles
         if (!commentPage.getTitleInCommentPageDelfi().startsWith(" no comments ")) {
             String titleTextInComments = commentPage.getTitleInCommentPageDelfi();
             System.out.println(titleTextInComments);
             Assertions.assertTrue(titleText.startsWith(titleTextInComments), "Titles are not the same on comment page");
         }
-        System.out.println("--------Printing all titles------------");
-        // get to console all titles
-        List<WebElement> titles = homePage.getTitlesOnDelfi();
-
-                int i = 0;
-        for (WebElement element : titles) {
-            System.out.println("Title " + i + ":" + element.getText());
-            i++;
-        }
-
-//        int i = 0;
-//        for (WebElement element : titles) {
-//            String text = element.getText();
-//            if (!text.isEmpty()) {
-//                element.getText();
-//            }
-//            System.out.println("Title " + i + ":" + text);
-//            i++;
-//        }
-
-
-
-//        for (int i = 0; i < titles.size(); i++){
-//            if (titles.size() > i){
-//                System.out.println(titles.get(i).getText());
-//            }
-//            System.out.println(titles.get(i).getText());
-
-//            if (comments.size() > i) {
-//                System.out.println(titles.get(i).getText() + " :: " + comments.get(i).getText());
-//            } else {
-//                System.out.println(titles.get(i).getText() + " ------ ");
-//            }
-//        }
-
-
-
-
     }
 
     @AfterEach
