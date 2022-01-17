3462196b25ef405a6f9cb74dd754eae645cf8bcd
Deniss Bubins <deniss1@inbox.ru>
2022-01-17 20:55:56 +0200

    Home work on TvNet page object.

--- src/test/java/pageobject/TvNetTestPageObject.java
+++ src/test/java/pageobject/TvNetTestPageObject.java
@@ -17,47 +17,34 @@ public class TvNetTestPageObject {
     @Test
     public void titlesAndCommentsCheck() {
 
-        //Open browser window (maximize it)
-
-        //Open home page
         baseFunc.openUrl(tvNetUrl);
         HomePage homePage = new HomePage(baseFunc);
 
-        //Accept cookies
         homePage.acceptCookies();
-        //Get 3rd article title and comments
         Article article = homePage.getArticleById(ArticleId);
         String titleTextOnHomePage = article.getTitle();
         System.out.println(article.getTitle());
         int commentCountOnHomePage = article.getCommentsCount();
         System.out.println(article.getCommentsCount());
 
-        //Open 3rd article
         homePage.openArticleByIdTVNET(ArticleId);
         ArticlePage articlePage = new ArticlePage(baseFunc);
-        //Get title
+
         String titleInArticle = articlePage.getTitleInArticleTVNET();
         System.out.println(titleInArticle);
-        //Get comments count
         int commentCountInArticle = articlePage.getCommentCountInArticleTVNET();
         System.out.println(commentCountInArticle);
-        //Compare titles
         Assertions.assertTrue(titleTextOnHomePage.startsWith(titleInArticle), "Title on article page is different");
-        //Compare comments count
         Assertions.assertEquals(commentCountOnHomePage, commentCountInArticle, "Comments count in article page is different");
-        //Open comment page
+
         articlePage.openCommentPageTVNET();
         CommentPage commentPage = new CommentPage(baseFunc);
 
-        //Get title
         String titleTextInCommentPage = commentPage.getTitleInCommentPageTVNET();
         System.out.println(titleTextInCommentPage);
-        //Get comments count
         int commentsCountInCommentPage = commentPage.getCommentsCountOnCommentPage();
         System.out.println(commentsCountInCommentPage);
-        //Compare titles
         Assertions.assertTrue(titleTextOnHomePage.startsWith(titleTextInCommentPage),"Title on comment page is different");
-        //Compare comments count
         Assertions.assertEquals(commentCountOnHomePage, commentsCountInCommentPage, "Comments count on comment page is different");
     }
 
