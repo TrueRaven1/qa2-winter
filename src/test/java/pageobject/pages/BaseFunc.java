3d6805a089cf8ee0f2d6c324849fc7d93ecdae5a
Deniss Bubins <deniss1@inbox.ru>
2022-02-16 21:01:35 +0200

    Selenium inputs and drops

--- src/test/java/pageobject/pages/BaseFunc.java
+++ src/test/java/pageobject/pages/BaseFunc.java
@@ -5,17 +5,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
 public class BaseFunc {
     private WebDriver browser;
+    private WebDriverWait wait;
 
     public BaseFunc() {
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
+        wait = new WebDriverWait(browser, Duration.ofSeconds(10));
     }
 
     public void openUrl(String url) {
@@ -26,7 +29,6 @@ public class BaseFunc {
     }
 
     public void click(By locator) {
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
 
@@ -35,7 +37,7 @@ public class BaseFunc {
     }
 
     public WebElement findElement(By locator) {
-        return browser.findElement(locator);
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
     public List<WebElement> findElements(WebElement parent, By child) {
@@ -43,7 +45,6 @@ public class BaseFunc {
     }
 
     public void clickByWebElement(WebElement we) {
-        WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(we)).click();
     }
 
@@ -58,5 +59,21 @@ public class BaseFunc {
 
     public void closeBrowser() {
         browser.close();
+    }
+
+    public void select(By locator, String value) {
+        Select select = new Select(findElement(locator));
+        select.selectByValue(value);
+    }
+
+    public void selectByVisibleText(By locator, String text){
+        Select select = new Select(findElement(locator));
+        select.selectByVisibleText(text);
+    }
+
+    public void type(By locator, String text) {
+        WebElement inputField = findElement(locator);
+        inputField.clear();
+        inputField.sendKeys(text);
     }
 }
